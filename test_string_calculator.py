6d3cdc9f734a483fc4005a3f18789543bcdb48b1
Hardeep.io <Hardeep0khalsa122@gmail.com>
2025-04-22 17:28:57 +0530

    TDD Step 2: Handle single number

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -7,6 +7,10 @@ class TestStringCalculator(unittest.TestCase):
     
     def test_empty_string(self):
         self.assertEqual(0, self.calculator.add(""))
+    
+    def test_single_number(self):
+        self.assertEqual(1, self.calculator.add("1"))
+        self.assertEqual(2, self.calculator.add("2"))
 
 if __name__ == "__main__":
     unittest.main() 
